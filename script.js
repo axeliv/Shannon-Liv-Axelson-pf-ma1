7daf4d5989f37d3f3b8a21636ea04df26ed8ecc6
axeliv <axeliv@users.noreply.github.com>
2020-05-26 13:40:01 +0200

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Q1: Declare a variable called firstVar but don't initialise it with a value.
+var firstVar;
+// Q2: Declare a variable called name and assign it your first name.
+var name = "Shannon Liv";
+// Q3: Declare and initialise a variable with a number value.
+var number = 12;
+// Q4: Create a variable called division and initialise it with a value of 20 divided by 5.
+var division = 20 / 5;
+// Q5: Write code that checks the type of the value "frog".
+console.log(typeof "frog");
+// Q6: Declare and initialise a variable called orderHasShipped with a boolean value.
+var orderHasShipped = true;
+// Q7: Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+if ((orderHasShipped = true)) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+// Q8: Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+for (var counter = 0; counter <= 9; counter++) {
+  console.log(counter);
+}
